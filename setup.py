d711ce0a9245fb8a9ed20ee052766e4a319b86f4
teratorn <teratorn /AT/ zoho /DOT/ com>
2016-10-27 00:52:00 -0700

    simple setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="epiphany-editor",
+      version="0.3",
+      description="Map Editor for Epiphany (a Boulder-Dash clone).",
+      author="Eric P. Mangold",
+      author_email="teratorn /AT/ zoho /DOT/ com",
+      scripts=['epiphany-editor'],
+      # depending on pygtk explicitly here is problematic in that it can't
+      # just be fetched from pypi and built like most dependencies.
+      #install_requires=['pygtk', 'pygame'] 
+      install_requires=['pygame']
+      )
