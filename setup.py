73680e92623255fe84d73172d01aadb9340113b8
teratorn <teratorn@teratorn>
2020-03-14 14:19:15 -0500

    port to python 3 and gtk 3 (pygobject)
    use subprocess.call() instead of os.system()
    update setup.py
    update README
    bump version

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup
 
 setup(name="epiphany-editor",
-      version="0.3.1",
+      version="0.4.2",
       description="Map Editor for Epiphany (a Boulder-Dash clone).",
-      author="Eric P. Mangold",
+      author="Eric Mangold",
       author_email="teratorn /AT/ zoho /DOT/ com",
       scripts=['epiphany-editor'],
-      # depending on pygtk explicitly here is problematic in that it can't
-      # just be fetched from pypi and built like most dependencies.
-      #install_requires=['pygtk', 'pygame'] 
-      install_requires=['pygame']
+      install_requires=['pygame', 'PyGObject']
       )
